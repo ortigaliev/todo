0becf4fc5b26c1d82ebb1e97b5fa0b2c57ccbc9f
OP-max <ortigaliyevm@gmail.com>
2025-06-04 14:01:06 +0900

    BRR: initial setup

---
+++ src/main/java/com/todo/todo1_1/Todo11Application.java
@@ -0,0 +1,13 @@
+package com.todo.todo1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Todo11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/todo/todo1_1/Todo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todo1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todo11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
